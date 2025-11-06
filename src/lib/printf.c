472e5b7a95987bdb8a8dd40bcdc209baaa33545c
jerry-debug-stack <1539368256@qq.com>
2025-11-06 13:38:53 +0800

    purify printf

--- src/lib/printf.c
+++ src/lib/printf.c
@@ -45,80 +45,28 @@ static uint32_t vsnprintf(char* buf,uint32_t size,const char* fmt,va_list args){
                 }
                 i++;
                 continue;
-            }else if(fmt[i]=='d'||fmt[i]=='i'){
-                signed_val=(int64_t)va_arg(args,int32_t);
-                goto parse_signed10;
-            }else if(fmt[i]=='u'){
-                unsigned_val=(uint64_t)va_arg(args,int32_t);
-                goto parse_unsigned10;
+            }else if (fmt[i] == '%'){
+                buf[pos++]='%';
+                i++;
+                continue;
             }else if (fmt[i] == 'p'){
                 unsigned_val=va_arg(args,uint64_t);
                 char_upper = 0;
                 with_header = 1;
                 goto parse_hex;
-            }else if (fmt[i] == 'x'){
-                unsigned_val=va_arg(args,uint32_t);
-                char_upper = 0;
-                goto parse_hex;
-            }else if (fmt[i] == 'X'){
-                unsigned_val=va_arg(args,uint32_t);
-                char_upper = 1;
-                goto parse_hex;
-            }else if (fmt[i] == 'o'){
-                unsigned_val=va_arg(args,uint32_t);
-                goto parse_octal;
-            }else if (fmt[i] == '%'){
-                buf[pos++]='%';
-                i++;
-                continue;
-            }else if (fmt[i] == 'h'){
-                i++;
-                if(fmt[i] == 'u'){
-                    unsigned_val=(uint16_t)va_arg(args,uint32_t);
-                    goto parse_unsigned10;
-                }else if(fmt[i] == 'd'||fmt[i] == 'i'){
-                    signed_val=(int16_t)va_arg(args,int32_t);
-                    goto parse_signed10;
-                }else if (fmt[i] == 'x'){
-                    unsigned_val=(uint16_t)va_arg(args,uint32_t);
-                    char_upper = 0;
-                    goto parse_hex;
-                }else if (fmt[i] == 'X'){
-                    unsigned_val=(uint16_t)va_arg(args,uint32_t);
-                    char_upper = 1;
-                    goto parse_hex;
-                }else if (fmt[i] == 'o'){
-                    unsigned_val=(uint16_t)va_arg(args,uint32_t);
-                    goto parse_octal;
-                }else{
-                    continue;
-                }
-            }else if (fmt[i] == 'l'){
-                i++;
-                if(fmt[i] == 'd'||fmt[i]=='i'){
-                    signed_val = va_arg(args,int64_t);
-                    goto parse_signed10;
-                }else if (fmt[i] == 'u'){
-                    unsigned_val=va_arg(args,uint64_t);
-                    goto parse_unsigned10;
-                }else if (fmt[i] == 'x'){
-                    unsigned_val=va_arg(args,uint64_t);
-                    char_upper = 0;
-                    goto parse_hex;
-                }else if (fmt[i] == 'X'){
-                    unsigned_val=va_arg(args,uint64_t);
-                    char_upper = 1;
-                    goto parse_hex;
-                }else if (fmt[i] == 'o'){
-                    unsigned_val=va_arg(args,uint64_t);
-                    goto parse_octal;
-                }else{
-                    continue;
-                }
             }else if (fmt[i] == '#'){
                 with_header = 1;
                 i++;
-                if (fmt[i] == 'x'){
+                goto parse_next;
+            }else{
+            parse_next:
+                if(fmt[i]=='d'||fmt[i]=='i'){
+                    signed_val=(int64_t)va_arg(args,int32_t);
+                    goto parse_signed10;
+                }else if(fmt[i]=='u'){
+                    unsigned_val=(uint64_t)va_arg(args,int32_t);
+                    goto parse_unsigned10;
+                }else if (fmt[i] == 'x'){
                     unsigned_val=va_arg(args,uint32_t);
                     char_upper = 0;
                     goto parse_hex;
@@ -176,8 +124,6 @@ static uint32_t vsnprintf(char* buf,uint32_t size,const char* fmt,va_list args){
                 }else{
                     continue;
                 }
-            }else{
-                continue;
             }
             parse_signed10:
                 if(signed_val<0){
